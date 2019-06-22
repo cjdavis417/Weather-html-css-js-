484546681889c6e187645a63c1ecc97f4d2895e2
Chris Davis <christopherjdavis417@gmail.com>
2019-06-22 12:29:20 -0400

    get weather function works

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,5 @@
-const apiKey = '6fb8c30fb929a4320dd57f04a61b8837';
 
-const URL = 'https://api.openweathermap.org/data/2.5/forecast'
-var zip = '40204';
-
-var requestURL = URL + '?zip=' + zip + ',us&APPID=6fb8c30fb929a4320dd57f04a61b8837';
-// var request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.responseType = 'json';
-// request.send();
-console.log(requestURL);
-document.getElementById('location').innerHTML = zip;
+var curLoc = document.getElementById('location');
 var curTime = document.getElementById('curTime');
 var curTemp = document.getElementById('curTemp');
 var curCond = document.getElementById('curCond');
@@ -17,19 +7,31 @@ var curHum = document.getElementById('curHum');
 var curWind = document.getElementById('curWind');
 var curDirection = document.getElementById('curDirection')
 
-fetch(requestURL)
-    .then(response => response.json())
-    .then(function (myJson) {
-        curTime.innerHTML = epochDate(myJson.list[0].dt);
-        curTemp.innerHTML = kel2far(myJson.list[0].main.temp);
-        curCond.innerHTML = myJson.list[0].weather[0].description;
-        curHum.innerHTML = myJson.list[0].main.humidity;
-        curWind.innerHTML = myJson.list[0].wind.speed;
-        curDirection.innerHTML = compassDir(myJson.list[0].wind.deg);
-    })
-        
-        
-        //myJson => curTime.innerHTML = epochDate(myJson.list[0].dt));
+
+
+function getWeather(zip) {
+    const URL = 'https://api.openweathermap.org/data/2.5/forecast'
+    var requestURL = URL + '?zip=' + zip + ',us&APPID=6fb8c30fb929a4320dd57f04a61b8837';
+
+    fetch(requestURL)
+        .then(response => response.json())
+        .then(function (myJson) {
+            curLoc.innerHTML = zip;
+            curTime.innerHTML = epochDate(myJson.list[0].dt);
+            curTemp.innerHTML = kel2far(myJson.list[0].main.temp);
+            curCond.innerHTML = myJson.list[0].weather[0].description;
+            curHum.innerHTML = myJson.list[0].main.humidity;
+            curWind.innerHTML = myJson.list[0].wind.speed;
+            curDirection.innerHTML = compassDir(myJson.list[0].wind.deg);
+        })
+}
+
+var form = document.getElementById('MyLocation');
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    var zipcode = document.getElementById('myLoc').value;
+    getWeather(zipcode);
+});
 
 
 // converting functions    
@@ -38,10 +40,12 @@ function epochDate(epoch) {
     return Date(epoch);
 }
 
+// convert K to F
 function kel2far(kelvin) {
     return Math.round((kelvin - 273.15)*9/5+32)
 }
 
+// convert degrees to compass direction
 function compassDir(degrees) {
     if (degrees == 360) {
         return 'N';
